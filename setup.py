20f4112b9dae45976a801f3ee4c77a296fe2f94c
Abhishek Minumula <minumuluaabhishek@gmail.com>
2024-09-21 17:08:32 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Abhishek Minumula',  
+    author_email='minumulaabhishek@gmail.com',  
+    packages=find_packages(),
+    install_requires=[]  
+)
+
